da8d6f8b7898585f463fc18c4ec1dd20fd353b48
happiness6533 <happiness6533@gmail.com>
2023-08-13 13:39:05 +0900

    feat: update assignment

--- assignment/assignment1.py
+++ assignment/assignment1.py
@@ -27,3 +27,27 @@ users = [user1, user2, user3]
 # …
 # 9 X 9 = 81
 # 위와 같은 수식을(구구단) 9줄 이하의 코드로 콘솔에 출력하세요
+
+# 과제3
+# feat/assignment-1 브랜치를 생성해주세요
+# 위의 과제를 feat/assignment-1 브랜치에서 수행하세요
+# 과제를 진행하는 도중에 저장해야 한다면 commit 하고 push 할 수 있습니다
+# 과제가 끝나면 저장한 후, master 브랜치로 이동해서 최신 버전을 pull 하세요
+# 다시 feat/assignment-1 브랜치로 이동한 후 master 브랜치를 현재 브랜치에 merge 하세요
+# 최종적으로 완성된 feat/assignment-1 브랜치를 push 하세요
+# 깃허브에서 pull request를 squash 모드로 실행하세요
+# 마지막으로 깃허브의 feat/assignment-1 브랜치와 로컬의 feat/assignment-1 브랜치를 모두 삭제하세요
+# 명령어는 아래를 참고하세요
+
+# 깃 명령어
+# git init: 임의의 폴더를 깃으로 관리하기 시작합니다
+# git add . : 현재 폴더의 모든 파일을 깃으로 관리합니다
+# git commit -m "feat: 메세지": 깃으로 작업 내용올 저장합니다
+# git push origin 브랜치: 깃허브 리포지토리에 브랜치를 업로드합니다
+# git clone url: 깃허브 리포지토리를 최초로 로컬에 복사합니다
+# git pull origin 브랜치: 깃허브 리포지토리에서 브랜치의 최신 버전을 로드합니다
+# git branch 브랜치: 브랜치를 생성합니다
+# git checkout 브랜치: 다른 브랜치로 이동합니다
+# git checkout 커밋아이다: 해당 시간대로 이동합니다
+# git merge 브랜치1 브랜치2: 브랜치2에서, 브랜치1의 내용을 브랜치2에 병합합니다
+# git branch -D 브랜치: 브랜치를 삭제합니다
