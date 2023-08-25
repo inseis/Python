6a58eb9ec88e8d6c71ae1b93291bc23aaa0332e9
inseis <inseo1563@naver.com>
2023-08-25 17:10:37 +0900

    change

--- assignment/assignment2/csv_file_manager.py
+++ assignment/assignment2/csv_file_manager.py
@@ -1,14 +1,22 @@
+import csv
 def read_csv_file(csv_file_name):
-    """ csv 파일을 읽고 모든 row를 배열에 담아서 리턴합니다
-    """
+    file_reader = open("users.csv", "r", encoding="utf8", newline="\n")
+    csv_file_reader = csv.reader(file_reader)
+    for row in csv_file_reader:
+        print(row)
     read_contents = []
     return read_contents
 
 
 def write_csv_file(csv_file_name, contents):
-    """ contents 배열에 들어있는 row를 csv 파일에 추가합니다
-    """
-    pass
+    file_writer = open("users.csv", "a", encoding="utf8", newline="\n")
+    csv_file_writer = csv.writer(file_writer)
+    email = input("이메일을 입력해주세요: ")
+    password = input("비밀번호를 입력해주세요: ")
+    nickname = input("별명을 입력해주세요: ")
+    csv_file_writer.writerow([email, password, nickname])
+    file_writer.close()
+
 
 
 if __name__ == "__main__":
--- assignment/assignment2/main.py
+++ assignment/assignment2/main.py
@@ -1,2 +1,63 @@
+from csv_file_manager import read_csv_file, write_csv_file
+import csv
+import random
+
 if __name__ == "__main__":
-    pass
+    csv_file_name = "users.csv"
+
+    while True:
+        email = input("이메일을 입력해주세요 (exit 입력 시 종료): ")
+        if email == "exit":
+            break
+        password = input("비밀번호를 입력해주세요: ")
+        nickname = input("별명을 입력해주세요: ")
+        information = [email, password, nickname]
+        write_csv_file(csv_file_name, information)
+
+        all_information = read_csv_file(csv_file_name)
+        print("모든 정보")
+        for information in all_information:
+            print(information)
+
+        random_users = []
+        if len(all_information) >= 6:
+            while len(random_users) < 2:
+                random_index = random.randint(0, len(all_information) - 1)
+                random_user = all_information[random_index]
+
+                is_user_selected = False
+                for user in random_users:
+                    if user == random_user:
+                        is_user_selected = True
+                        break
+
+                if is_user_selected is False:
+                    random_users.append(random_user)
+
+            print("랜덤 추출 2명:")
+            for random_user in random_users:
+                print(random_user[2])  # 사용자의 이름 출력
+            break
+
+
+
+
+
+
+
+
+
+
+
+# csv_file_manager.py 파일의 함수 2개를 완성하세요
+# main.py 파일에서 csv_file_manager.py 파일의 함수 2개를 가져와서 코드를 작성하세요
+# main.py 파일에서 아래의 유저들을 콘솔에서 입력받고, csv 파일에 데이터를 쓸 수 있도록 코드를 작성하세요
+# 유저의 이메일을 입력받을 때, exit를 입력하면 프로그램이 종료되도록 만들어주세요
+# 유저 1명이 추가될 때마다, 현재 csv 파일에 저장된 모든 유저들의 목록을 전부 출력해주세요
+# 추가할 유저 정보는 이메일, 비밀번호, 이름으로 아래와 같습니다
+# xxx@xxx.com | 1234 | 김세모
+# zzz@zzz.com | 1234 | 김동그라미
+# qqq@qqq.com | 1234 | 쵸단
+# www@www.com | 1234 | 마젠타
+# eee@eee.com | 1234 | 냥뇽녕냥
+# 마지막으로 6명의 유저를 모두 입력받은 후 랜덤으로 2명의 유저를 선택해서 이름만 출력해주세요
